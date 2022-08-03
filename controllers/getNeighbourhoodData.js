abd9458cf2e3da003a167fa8a7cb7738eea8104a
azotamiota <azotamiota@gmail.com>
2022-08-03 16:34:24 +0100

    add Neighbourhood model

---
+++ controllers/getNeighbourhoodData.js
@@ -0,0 +1,55 @@
+const Neighbourhood = require('../models/Neighbourhood')
+
+function showPeople () {
+        try {
+        const allPeople = Neighbourhood.showPeople();
+        res.json(allPeople);
+    } catch (e) {
+        res.status(500).json({ error: e.message });
+    }// TODO
+    }
+
+function showHouses() {
+        try {
+        const allHouses = Neighbourhood.showHouses();
+        res.json(allHouses);
+    } catch (e) {
+        res.status(500).json({ error: e.message });
+    }// TODO        
+    }
+
+function showOnePerson() {
+        try {
+        const onePerson = Neighbourhood.showOnePerson();
+        res.json(onePerson);
+    } catch (e) {
+        res.status(500).json({ error: e.message });
+    }// TODO
+    }
+
+function showOneHouse() {
+        try {
+        const oneHouse = Neighbourhood.showOneHouse();
+        res.json(oneHouse);
+    } catch (e) {
+        res.status(500).json({ error: e.message });
+    }// TODO
+    }
+
+function showPeopleWithinAgeRange() {
+        try {
+        const peopleWithinAge = Neighbourhood.showPeopleWithinAgeRange();
+        res.json(peopleWithinAge);
+    } catch (e) {
+        res.status(500).json({ error: e.message });
+    }// TODO
+    }
+
+function showPeopleByHouseholdMembers () {
+        try {
+        const peopleByHousholdMembers = Neighbourhood.showPeopleByHouseholdMembers();
+        res.json(peopleByHousholdMembers);
+    } catch (e) {
+        res.status(500).json({ error: e.message });
+    }// TODO
+    }
---
+++ models/Neighbourhood.js
@@ -0,0 +1,34 @@
+const neighbourhood = require('../data')
+
+class Neighbourhood {
+    constructor(data) {
+        // TODO
+    }
+
+    static showPeople() {
+        // TODO
+    }
+
+    static showHouses() {
+    // TODO        
+    }
+
+    static showOnePerson() {
+        // TODO
+    }
+
+    static showOneHouse() {
+        // TODO
+    }
+
+    static showPeopleWithinAgeRange() {
+        // TODO
+    }
+
+    static showPeopleByHouseholdMembers () {
+        // TODO
+    }
+
+}
+
+module.exports = Neighbourhood
