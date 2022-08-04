e71b4ce5ad6598747e58e6dd9687af65f3cbf65e
azotamiota <azotamiota@gmail.com>
2022-08-04 19:43:01 +0100

    setup first model

--- api/models/Neighbourhood.js
+++ api/models/Neighbourhood.js
@@ -1,12 +1,25 @@
-const neighbourhoodData = require('../data/neighboursData')
+const neighbourhoodData = require('../database/connect_postgres')
 
 class Neighbourhood {
     constructor(data) {
-        // TODO
+        this.id = data.id// TODO
+        this.houseid = data.houseid// TODO
+        this.name = data.name// TODO
+        this.age = data.age// TODO
+        this.householdPeople = data.householdPeople
     }
 
     static showPeople() {
-        // TODO
+       return new Promise (async (resolve, reject) => {
+        try {
+            const peopleData = await neighbourhoodData.query('SELECT * FROM people')
+            const people = peopleData.rows.map(person => new Neighbourhood(person))
+            resolve (people)
+        } catch (error) {
+            console.log('error: ', error);
+            reject('Unable to connect database')
+        }
+       })
     }
 
     static showHouses() {
